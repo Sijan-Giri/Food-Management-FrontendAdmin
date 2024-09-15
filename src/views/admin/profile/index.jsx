00e8f012ca2986e9bb6446cc504adc36ffd59480
Sijan-Giri <girisijan346@gmail.com>
2024-09-15 14:10:55 +0545

    Admin Dashboard

---
+++ src/views/admin/profile/index.jsx
@@ -0,0 +1,11 @@
+
+
+const ProfileOverview = () => {
+  return (
+      <>
+      <h1>profile section</h1>
+      </>
+  );
+};
+
+export default ProfileOverview;
---
+++ src/views/admin/tables/index.jsx
@@ -0,0 +1,11 @@
+
+
+const Tables = () => {
+  return (
+    <>
+    <h1>Product section soon...</h1>
+    </>
+  );
+};
+
+export default Tables;
